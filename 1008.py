e135c685c1ea2d2476019bbebe0ee72388d9b2ae
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-07-14 19:49:01 -0300

    Create 1008.py

---
+++ 1008.py
@@ -0,0 +1,7 @@
+numfunc = int(input())
+hrtrab = int(input())
+valorhr = float(input())
+salario = float(hrtrab * valorhr)
+print(f'NUMBER = {numfunc}')
+print(f'SALARY = U$ {salario:.2f}')
+print(end='')
