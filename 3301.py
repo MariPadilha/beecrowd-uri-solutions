1875dc1d227a3871013e41a7548ef5fe21992186
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-07-23 14:56:16 -0300

    Create 3301.py

---
+++ 3301.py
@@ -0,0 +1,13 @@
+H, Z, L = input().split(' ')
+if Z > H and Z < L:
+    print('zezinho')
+if Z < H and Z > L:
+    print('zezinho')
+if L > H and L < Z:
+    print('luisinho')
+if L < H and L > Z:
+    print('luisinho')
+if H > Z and H < L:
+    print('huguinho')
+if H < Z and H > L:
+    print('huguinho')
