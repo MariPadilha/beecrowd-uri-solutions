9bf198e1a92765243752c0ae1bc912edf43028cb
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-08-04 22:33:08 -0300

    Create 2344.py

---
+++ 2344.py
@@ -0,0 +1,11 @@
+N = int(input())
+if N == 0:
+    print('E')
+if N >= 1 and N <=35:
+    print('D')
+if N >= 36 and N <=60:
+    print('C')
+if N >= 61 and N <=85:
+    print('B')
+if N >= 86 and N <= 100:
+    print('A')
