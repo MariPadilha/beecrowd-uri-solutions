61a6d6d08698c5cf278fbff99e713ea676d9e993
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-09-04 00:07:20 -0300

    Create 2791.py

---
+++ 2791.py
@@ -0,0 +1,13 @@
+a, b, c, d = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+if a == 1:
+    print('1')
+if b == 1:
+    print('2')
+if c == 1:
+    print('3')
+if d == 1:
+    print('4')
