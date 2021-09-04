3ddf4dd3dc1e093233da2a67e44f498ee3fcacb3
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-09-04 01:34:22 -0300

    Create 2987.py

---
+++ 2987.py
@@ -0,0 +1,53 @@
+a = input().lower()
+if a == 'a':
+    print('1')
+if a == 'b':
+    print('2')
+if a == 'c':
+    print('3')
+if a == 'd':
+    print('4')
+if a == 'e':
+    print('5')
+if a == 'f':
+    print('6')
+if a == 'g':
+    print('7')
+if a == 'h':
+    print('8')
+if a == 'i':
+    print('9')
+if a == 'j':
+    print('10')
+if a == 'k':
+    print('11')
+if a == 'l':
+    print('12')
+if a == 'm':
+    print('13')
+if a == 'n':
+    print('14')
+if a == 'o':
+    print('15')
+if a == 'p':
+    print('16')
+if a == 'q':
+    print('17')
+if a == 'r':
+    print('18')
+if a == 's':
+    print('19')
+if a == 't':
+    print('20')
+if a == 'u':
+    print('21')
+if a == 'v':
+    print('22')
+if a == 'w':
+    print('23')
+if a == 'x':
+    print('24')
+if a == 'y':
+    print('25')
+if a == 'z':
+    print('26')
