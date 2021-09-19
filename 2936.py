febcf836eca8d4624fea26ebcc08a3bf2307eb2a
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-09-19 18:55:09 -0300

    Create 2936.py

---
+++ 2936.py
@@ -0,0 +1,16 @@
+curupira = 300
+boitata = 1500
+boto = 600
+mapinguari = 1000
+lara = 150
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+ai = a * curupira
+bi = b * boitata
+ci = c * boto
+di = d * mapinguari
+ei = e * lara
+print(ai + bi + ci +di+ei+225)
