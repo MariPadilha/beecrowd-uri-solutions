35a1e8f51907c2faf2bc77790906ae6ccfab4c14
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-10-10 00:10:35 -0300

    Create 1002.js

---
+++ 1002.js
@@ -0,0 +1,5 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var n1 = Number(lines.shift())
+var area = 3.14159 * n1 ** 2
+console.log(`A=${area.toFixed(4)}`)
