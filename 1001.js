dab4e3c288e4ae8d22be297f4c828d64cd24f190
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-10-10 00:12:49 -0300

    Update 1001.js

--- 1001.js
+++ 1001.js
@@ -1,5 +1,5 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
-var n1 = Number(lines.shift())
-var n2 = Number(lines.shift())
-console.log(`X = ${n1 + n2}`)
+var n1 = Number(lines.shift());
+var n2 = Number(lines.shift());
+console.log(`X = ${n1 + n2}`);
