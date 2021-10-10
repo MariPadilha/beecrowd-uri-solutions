c7c572e2d78837bba04c1033a454c0932980d27a
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-10-10 00:05:44 -0300

    Create 1001.js

---
+++ 1001.js
@@ -0,0 +1,5 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var n1 = Number(lines.shift())
+var n2 = Number(lines.shift())
+console.log(`X = ${n1 + n2}`)
