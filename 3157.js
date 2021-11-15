01f2d9d39188a608c2781fc18b9852db8dacf850
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-11-15 19:39:40 -0300

    Create 3157.js

---
+++ 3157.js
@@ -0,0 +1,6 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var A = lines.shift();
+var S = A.length
+if(0 < S <= 280){
+    console.log('Nao se refuta ciencia com opiniao')}
