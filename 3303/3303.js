b972850f0fe2a34817ab78ade451593c72ce7878
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-11-14 23:52:55 -0300

    Delete 3303 directory

--- 3303/3303.js
+++
@@ -1,8 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-var palavra = lines.shift();
-var n = palavra.length
-if (n >= 10){
-    console.log('palavrao')}
-else if (n < 10){
-    console.log('palavrinha')}
