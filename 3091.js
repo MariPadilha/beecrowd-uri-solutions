47b67d6f7256ef1e5ee15b9709f89c90f54b6ed7
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-11-18 21:04:17 -0300

    Create 3091.js

---
+++ 3091.js
@@ -0,0 +1,5 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var A = parseInt(lines.shift())
+var B = parseInt(lines.shift())
+console.log(`${A%B}`);
