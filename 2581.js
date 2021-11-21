627a70b7988d09ead26c409f6a7103715f68b14d
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-11-21 20:52:59 -0300

    Create 2581.js

---
+++ 2581.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var n = Number(lines.shift())
+for(var i = 0;i < n;i++){
+    var g = lines.shift()}
+for(var j = 0;j < n;j++){
+    console.log('I am Toorg!')}
