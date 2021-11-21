5b8c589fa43634d40f87ebb8aa257e04dfb079e3
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-11-21 20:06:12 -0300

    Create 2780.js

---
+++ 2780.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var D = Number(lines.shift())
+if(D >= 0 && D <= 2000){
+    if(D <= 800){
+        console.log('1')}
+    else if(800 < D && D <= 1400){
+        console.log('2')}
+    else if(1400 < D && D <= 2000){
+        console.log('3')}}
