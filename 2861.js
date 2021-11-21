5af68d009014e682c3596375ed7b3671f8e3f0ac
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2021-11-21 19:45:41 -0300

    Create 2861.js

---
+++ 2861.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var C = Number(lines.shift())
+if(2 <= C <= 99){
+    for(var d = 0;d < C; d++){
+        var d = lines.shift()}
+    for(var e = 0;e < C; e++){
+        console.log('gzuz')}}
