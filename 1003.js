fb8313f20cc19f133438bbc1206a25561773672c
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2022-03-26 22:12:28 -0300

    Update 1003.js

--- 1003.js
+++ 1003.js
@@ -1,5 +1,5 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-var n1 = Number(lines.shift());
-var n2 = Number(lines.shift());
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+let n1 = Number(lines.shift());
+let n2 = Number(lines.shift());
 console.log(`SOMA = ${n1+n2}`);
