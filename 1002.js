4e8acc5664ab0d92368488b2ec907e947720803f
k0sn1k4 <87348445+k0sn1k4@users.noreply.github.com>
2022-03-26 22:11:52 -0300

    Update 1002.js

--- 1002.js
+++ 1002.js
@@ -1,5 +1,5 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-var n1 = Number(lines.shift());
-var area = 3.14159 * n1 ** 2;
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+let n1 = Number(lines.shift());
+let area = 3.14159 * n1 ** 2;
 console.log(`A=${area.toFixed(4)}`);
