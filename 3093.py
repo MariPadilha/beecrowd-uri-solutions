c4085a32579e4c1202438caf7262a46596cd3ca9
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2022-08-06 16:20:40 -0300

    Create 3093.py

---
+++ 3093.py
@@ -0,0 +1,4 @@
+n = int(input())
+for i in range(0, n):
+    a = input()
+    print('Aaah muleke') if a.count('Q') != 0 and a.count('J') != 0 and a.count('K') != 0 and a.count('A') != 0 else print('Ooo raca viu')
