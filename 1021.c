60524a0d5fef1e572d8c91595bbc15823d262e7e
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2022-08-12 21:48:35 -0300

    Create 1021.c

---
+++ 1021.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main(){
+    float n1, n;
+    int valor100, valor50, valor20, valor10, valor5, valor2, valor1, valor050, valor025, valor010, valor05;
+    scanf("%f", &n1);
+    n = n1;
+    valor100 = n / 100;
+    n = n - valor100*100;
+    valor50 = n / 50;
+    n = n - valor50*50;
+    valor20 = n / 20;
+    n = n - valor20*20;
+    valor10 = n / 10;
+    n = n - valor10*10;
+    valor5 = n / 5;
+    n = n - valor5*5;
+    valor2 = n / 2;
+    n = n - valor2*2;
+    valor1 = n / 1;
+    n = n - valor1*1;
+    valor050 = n / 0.50;
+    n = n - valor050*0.50;
+    valor025 = n / 0.25;
+    n = n - valor025*0.25;
+    valor010 = n / 0.10;
+    n = n - valor010*0.10;
+    valor05 = n / 0.05;
+    n = n - valor05*0.05;
+    printf("NOTAS:\n");
+    printf("%i nota(s) de R$ 100.00\n", valor100);
+    printf("%i nota(s) de R$ 50.00\n", valor50);
+    printf("%i nota(s) de R$ 20.00\n", valor20);
+    printf("%i nota(s) de R$ 10.00\n", valor10);
+    printf("%i nota(s) de R$ 5.00\n", valor5);
+    printf("%i nota(s) de R$ 2.00\n", valor2);
+    printf("MOEDAS:\n");
+    printf("%i moeda(s) de R$ 1.00\n", valor1);
+    printf("%i moeda(s) de R$ 0.50\n", valor050);
+    printf("%i moeda(s) de R$ 0.25\n", valor025);
+    printf("%i moeda(s) de R$ 0.10\n", valor010);
+    printf("%i moeda(s) de R$ 0.05\n", valor05);
+    printf("%.0f moeda(s) de R$ 0.01\n", n*100);
+}
