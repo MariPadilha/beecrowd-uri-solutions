ec91b78d31d440657b7fe2c4954a3f62027a30ce
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2022-09-26 21:01:41 -0300

    Create 1189.py

---
+++ 1189.py
@@ -0,0 +1,20 @@
+tipo = input()
+matriz=[]
+linha=[]
+for i in range(0, 12):
+    for j in range(0, 12):
+        linha+=[float(input())]
+    matriz+=[linha[:]]
+    linha.clear()
+soma = itens = 0
+a = 1; b = 11; l = 0
+for p in range(0, 5):
+    for i in range(a, b):
+        soma += matriz[i][l]
+        itens += 1
+    l += 1
+    a += 1
+    b -= 1
+if tipo == 'M':
+    soma = soma/itens
+print(f'{soma:.1f}')
