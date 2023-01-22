5c3869b045f841d90827b4ed9da180db854a31ea
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2023-01-22 14:24:50 -0300

    Create 1924.js

---
+++ 1924.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+let n = parseInt(lines.shift());
+for(var i = 0; i < n; i++){
+    let d = lines.shift();
+}
+console.log('Ciencia da Computacao');
