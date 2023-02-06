bbe70349117aaacaeecd3ff1d7e6ef610fed1b0c
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2023-02-06 00:33:27 -0300

    Create 1007.cs

---
+++ 1007.cs
@@ -0,0 +1,10 @@
+using System; 
+class URI {
+    static void Main(string[] args) { 
+        int A = int.Parse(Console.ReadLine()); 
+        int B = int.Parse(Console.ReadLine()); 
+        int C = int.Parse(Console.ReadLine()); 
+        int D = int.Parse(Console.ReadLine());
+        Console.WriteLine($"DIFERENCA = {A * B - C * D}");
+    }
+}
