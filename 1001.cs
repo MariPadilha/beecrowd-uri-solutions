f8352368489adca2edf1302e09bf2d3e68ae4332
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2023-02-06 00:06:35 -0300

    Create 1001.cs

---
+++ 1001.cs
@@ -0,0 +1,7 @@
+using System; 
+class URI {
+    static void Main(string[] args) { 
+        int n1 = int.Parse(Console.ReadLine()); int n2 = int.Parse(Console.ReadLine());
+        Console.WriteLine($"X = {n1+n2}");
+    }
+}
