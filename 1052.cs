89332e1ce288e05caf5308c0b903669f9a454b93
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2023-02-14 14:52:55 -0300

    Create 1052.cs

---
+++ 1052.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Globalization;
+class URI {
+    static void Main(string[] args) { 
+        int mes = int.Parse(Console.ReadLine());
+        if(mes == 1){
+            Console.WriteLine("January");
+        }
+        else if(mes == 2){
+            Console.WriteLine("February");
+        }
+        else if(mes == 3){
+            Console.WriteLine("March");
+        }
+        else if(mes == 4){
+            Console.WriteLine("April");
+        }
+        else if(mes == 5){
+            Console.WriteLine("May");
+        }
+        else if(mes == 6){
+            Console.WriteLine("June");
+        }
+        else if(mes == 7){
+            Console.WriteLine("July");
+        }
+        else if(mes == 8){
+            Console.WriteLine("August");
+        }
+        else if(mes == 9){
+            Console.WriteLine("September");
+        }
+        else if(mes == 10){
+            Console.WriteLine("October");
+        }
+        else if(mes == 11){
+            Console.WriteLine("November");
+        }
+        else if(mes == 12){
+            Console.WriteLine("December");
+        }
+    }
+}
