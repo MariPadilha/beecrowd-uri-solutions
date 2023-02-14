973f593bb05d71aab9cc0436f2549bd99e9ed1ba
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2023-02-14 18:54:49 -0300

    Create 1007.cpp

---
+++ 1007.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int A, B, C, D;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> D;
+    cout << "DIFERENCA = " << A * B - C * D << endl;
+}
