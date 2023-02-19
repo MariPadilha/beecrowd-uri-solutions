540d6cde957df32d3ade5d0266b50cbe967929b3
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2023-02-18 23:55:59 -0300

    Create 1052.cpp

---
+++ 1052.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int mes;
+    cin >> mes;
+    if(mes == 1){
+        cout << "January" << endl;
+    }
+    else if(mes == 2){
+        cout << "February" << endl;
+    }
+    else if(mes == 3){
+        cout << "March" << endl;
+    }
+    else if(mes == 4){
+        cout << "April" << endl;
+    }
+    else if(mes == 5){
+        cout << "May" << endl;
+    }
+    else if(mes == 6){
+        cout << "June" << endl;
+    }
+    else if(mes == 7){
+        cout << "July" << endl;
+    }
+    else if(mes == 8){
+        cout << "August" << endl;
+    }
+    else if(mes == 9){
+        cout << "September" << endl;
+    }
+    else if(mes == 10){
+        cout << "October" << endl;
+    }
+    else if(mes == 11){
+        cout << "November" << endl;
+    }
+    else if(mes == 12){
+        cout << "December" << endl;
+    }
+    return 0;
+}
