495c45f4aa8acc71fa818b03350fdc12952a941a
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2023-02-24 13:57:12 -0300

    Create 1078.cpp

---
+++ 1078.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int a = 1; a < 11; a++){
+        cout << a << " x " << n << " = " << n*a << "\n";
+    }
+}
