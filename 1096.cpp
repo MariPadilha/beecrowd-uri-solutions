0a02df06f5f3e53db40cb76876bedaedda6533fc
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2023-02-25 13:06:22 -0300

    Create 1096.cpp

---
+++ 1096.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "I=1 J=7\nI=1 J=6\nI=1 J=5\nI=3 J=7\nI=3 J=6\nI=3 J=5\nI=5 J=7\nI=5 J=6\nI=5 J=5\nI=7 J=7\nI=7 J=6\nI=7 J=5\nI=9 J=7\nI=9 J=6\nI=9 J=5\n";
+}
