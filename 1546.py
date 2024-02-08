57135c84c211c7f420e74ba28a5c6274a5c3b0ab
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2024-02-08 18:09:09 -0300

    Create 1546.py

---
+++ 1546.py
@@ -0,0 +1,11 @@
+for i in range(0, int(input())):
+    for j in range(0, int(input())):
+        n = int(input())
+        if n == 1:
+            print('Rolien')
+        if n == 2:
+            print('Naej')
+        if n == 3:
+            print('Elehcim')
+        if n == 4:
+            print('Odranoel')
