e7400c71048ddc6d6bdb1cd1d63d8bc6217bce8d
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2024-03-04 20:01:01 -0300

    Create 1016.java

---
+++ 1016.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int b = 60*n/30;
+        System.out.println(b+" minutos");
+        }
+}
