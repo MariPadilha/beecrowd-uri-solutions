07420a7a4fc9aae2c856b123a1b7f1c77a02d943
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2024-03-15 20:05:23 -0300

    Create 1097.java

---
+++ 1097.java
@@ -0,0 +1,19 @@
+public class Main {
+public static void main(String[] args) {
+        System.out.println("I=1 J=7\n" +
+                "I=1 J=6\n" +
+                "I=1 J=5\n" +
+                "I=3 J=9\n" +
+                "I=3 J=8\n" +
+                "I=3 J=7\n" +
+                "I=5 J=11\n" +
+                "I=5 J=10\n" +
+                "I=5 J=9\n" +
+                "I=7 J=13\n" +
+                "I=7 J=12\n" +
+                "I=7 J=11\n" +
+                "I=9 J=15\n" +
+                "I=9 J=14\n" +
+                "I=9 J=13");
+}
+}
