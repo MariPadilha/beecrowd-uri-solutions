ce85da42ccc02e8a5dff15a9b87ed3bf62e1ecf6
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2024-03-15 19:59:14 -0300

    Create 1095.java

---
+++ 1095.java
@@ -0,0 +1,17 @@
+public class Main {
+public static void main(String[] args) {
+        System.out.println("I=1 J=60\n" +
+                "I=4 J=55\n" +
+                "I=7 J=50\n" +
+                "I=10 J=45\n" +
+                "I=13 J=40\n" +
+                "I=16 J=35\n" +
+                "I=19 J=30\n" +
+                "I=22 J=25\n" +
+                "I=25 J=20\n" +
+                "I=28 J=15\n" +
+                "I=31 J=10\n" +
+                "I=34 J=5\n" +
+                "I=37 J=0");
+}
+}
