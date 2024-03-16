fdeb99c1ddbfd316e691bd1cd291ad13e438f35c
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2024-03-15 23:54:43 -0300

    Create 3344.java

---
+++ 3344.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(4);
+}
+}
