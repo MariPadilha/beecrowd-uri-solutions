5d0dc63aac100d23fbb9e00bfa7fb25711bf8d8d
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2024-03-17 20:04:52 -0300

    Create 1144.java

---
+++ 1144.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        for(int i = 1; i < a+1; i++) {
+                int b = (int) Math.pow(i,2);
+                int c = b * i;
+                System.out.println(i + " " + b + " " + c);
+                System.out.println(i + " " + (b+1) + " " + (c+1));
+        }
+}
+}
