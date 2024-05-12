b429e0703103c8b139d4d606ffb929f6a22bade1
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2024-05-12 19:31:12 -0300

    Create 2760.py

---
+++ 2760.py
@@ -0,0 +1,7 @@
+frase1 = input()
+frase2 = input()
+frase3 = input()
+print(frase1+frase2+frase3)
+print(frase2+frase3+frase1)
+print(frase3+frase1+frase2)
+print(frase1[:10]+frase2[:10]+frase3[:10])
