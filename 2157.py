1d7342856a0d392d6770e2f80f156c392a2ef3ce
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2024-10-13 15:38:54 -0300

    Delete 2157.py

--- 2157.py
+++
@@ -1,7 +0,0 @@
-for i in range(0, int(input())):
-    menor, maior = map(int, input().split(" "))
-    sequencia = ""
-    for j in range(menor, maior+1):
-        sequencia += str(j)
-    sequencia+=sequencia[::-1]
-    print(sequencia)
