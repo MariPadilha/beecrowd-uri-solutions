0483e082dbd983e892b44fa8859e0ef688f22653
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2024-11-14 00:35:37 -0300

    Create 1097.c

---
+++ 1097.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("I=1 J=7\nI=1 J=6\nI=1 J=5\nI=3 J=9\nI=3 J=8\nI=3 J=7\nI=5 J=11\nI=5 J=10\nI=5 J=9\nI=7 J=13\nI=7 J=12\nI=7 J=11\nI=9 J=15\nI=9 J=14\nI=9 J=13\n");
+    return 0;
+}
