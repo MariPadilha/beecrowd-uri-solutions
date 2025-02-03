5172326a2f8f7a6a08140f5d23788ed89accf8c0
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2025-02-02 23:22:57 -0300

    Create 1924.cpp

---
+++ 1924.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int numero_casos;
+    string curso;
+    cin >> numero_casos;
+    for(int i = 0; i<numero_casos;i++){
+        cin>>curso;
+    }
+    cout << "Ciencia da Computacao\n";
+    return 0;
+}
