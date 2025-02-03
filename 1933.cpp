621d8432f78b87944ac54aa05501e82904f958d5
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2025-02-03 00:06:09 -0300

    Create 1933.cpp

---
+++ 1933.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int carta1, carta2;
+    cin >> carta1 >> carta2;
+    cout << max(carta1, carta2) << endl;
+    return 0;
+}
