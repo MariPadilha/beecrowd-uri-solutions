e2a1ef27c28fba33c8238bd06507cc8781c3e602
MariPadilha <87348445+MariPadilha@users.noreply.github.com>
2025-02-15 23:28:19 -0300

    Create 2234.cpp

---
+++ 2234.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    double a,b;
+    cin>>a>>b;
+    cout << fixed << setprecision(2);
+    cout << a/b << endl;
+    return 0;
+}
